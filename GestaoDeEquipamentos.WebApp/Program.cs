fb17e62b3bd7a566a3fc60282c27a9dcd4617d5b
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-08-20 20:30:45 -0300

    Adiciona código compartilhado e estrutura do CSHTML

--- GestaoDeEquipamentos.WebApp/Program.cs
+++ GestaoDeEquipamentos.WebApp/Program.cs
@@ -1,6 +1,21 @@
+using GestaoDeEquipamentos.WebApp.Compartilhado.Apresesntacao;
+using GestaoDeEquipamentos.WebApp.Compartilhado.Infraestrutura;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar a infraestrutura (Arquivos, Banco de Dados, Logs, Cachês, etc...)
+builder.Services.AdicionarCamadaDeInfraestrutura();
+
+// Configurar o MVC / Apresentação
+builder.Services.AdicionarCamadaDeApresentacao();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Middlewares
+app.UseRouting();
+app.MapDefaultControllerRoute();
 
+app.UseStaticFiles();
+
+// Executa o servidor
 app.Run();
