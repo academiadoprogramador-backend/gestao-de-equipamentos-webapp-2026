c5f1a9d00b737a6733757ccf6a1d6fc5e9b347f7
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-09-08 21:31:20 -0300

    Adiciona injeção de dependência e CRUD de Fabricantes em SQL

--- GestaoDeEquipamentos.WebApp/Modulos/Fabricantes/Infraestrutura/RepositorioFabricanteEmSql.cs
+++ GestaoDeEquipamentos.WebApp/Modulos/Fabricantes/Infraestrutura/RepositorioFabricanteEmSql.cs
@@ -1,31 +1,94 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
 using GestaoDeEquipamentos.WebApp.Modulos.Fabricantes.Dominio;
 
 namespace GestaoDeEquipamentos.WebApp.Modulos.Fabricantes.Infraestrutura;
 
 public sealed class RepositorioFabricanteEmSql : IRepositorioFabricante
 {
+    private readonly string connectionString;
+
+    public RepositorioFabricanteEmSql(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
     public void Cadastrar(Fabricante novoRegistro)
     {
-        throw new NotImplementedException();
+        const string query =
+            """
+            INSERT INTO dbo.TBFabricantes (Nome, Email, Telefone)
+            OUTPUT INSERTED.Id
+            VALUES (@Nome, @Email, @Telefone)
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        novoRegistro.Id = conexao.QuerySingle<int>(query, novoRegistro);
     }
 
     public bool Editar(int idSelecionado, Fabricante entidadeAtualizada)
     {
-        throw new NotImplementedException();
+        const string query =
+            """
+            UPDATE dbo.TBFabricantes
+            SET Nome = @Nome,
+                Email = @Email,
+                Telefone = @Telefone
+            WHERE Id = @Id
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        int quantidadeRegistrosAlterados = conexao.Execute(query, new
+        {
+            Id = idSelecionado,
+            entidadeAtualizada.Nome,
+            entidadeAtualizada.Email,
+            entidadeAtualizada.Telefone
+        });
+
+        return quantidadeRegistrosAlterados == 1;
     }
 
     public bool Excluir(int idSelecionado)
     {
-        throw new NotImplementedException();
+        const string query = "DELETE FROM dbo.TBFabricantes WHERE Id = @Id";
+
+        using SqlConnection conexao = new(connectionString);
+
+        int quantidadeRegistrosExcluidos = conexao.Execute(query, new { Id = idSelecionado });
+
+        return quantidadeRegistrosExcluidos == 1;
     }
 
     public Fabricante? SelecionarPorId(int idSelecionado)
     {
-        throw new NotImplementedException();
+        const string query =
+            """
+            SELECT Id, Nome, Email, Telefone
+            FROM dbo.TBFabricantes
+            WHERE Id = @Id
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        // Criação de um objeto anônimo
+        return conexao.QuerySingleOrDefault<Fabricante>(query, new { Id = idSelecionado });
     }
 
     public List<Fabricante> SelecionarTodos()
     {
-        throw new NotImplementedException();
+        const string query =
+            """
+            SELECT Id, Nome, Email, Telefone
+            FROM dbo.TBFabricantes
+            ORDER BY Id
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        // Query = Consulta no banco (SQL = Structured Query Language)
+        return conexao.Query<Fabricante>(query).ToList();
     }
 }
--- GestaoDeEquipamentos.WebApp/Program.cs
+++ GestaoDeEquipamentos.WebApp/Program.cs
@@ -4,7 +4,7 @@ using GestaoDeEquipamentos.WebApp.Compartilhado.Infraestrutura;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configurar a infraestrutura (Arquivos, Banco de Dados, Logs, Cachês, etc...)
-builder.Services.AdicionarCamadaDeInfraestrutura();
+builder.Services.AdicionarCamadaDeInfraestrutura(builder.Configuration);
 
 // Configurar o MVC / Apresentação
 builder.Services.AdicionarCamadaDeApresentacao();
