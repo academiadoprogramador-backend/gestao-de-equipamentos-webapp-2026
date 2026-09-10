666df2d252a80a8595be9995c7414647d92878a8
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-09-10 18:31:59 -0300

    Organiza classe Equipamento

--- GestaoDeEquipamentos.WebApp/Modulos/Equipamentos/Dominio/Equipamento.cs
+++ GestaoDeEquipamentos.WebApp/Modulos/Equipamentos/Dominio/Equipamento.cs
@@ -9,18 +9,20 @@ public sealed class Equipamento : EntidadeBase
     public DateTime DataFabricacao { get; set; }
     public Fabricante Fabricante { get; set; } = null!;
 
-    public Equipamento()
-    {
-    }
+    public Equipamento() { }
 
-    public Equipamento(string nome, decimal precoAquisicao, DateTime dataFabricacao, Fabricante fabricante)
+    public Equipamento(
+        string nome,
+        decimal precoAquisicao,
+        DateTime dataFabricacao,
+        Fabricante fabricante
+    )
     {
         Nome = nome;
         PrecoAquisicao = precoAquisicao;
         DataFabricacao = dataFabricacao;
         Fabricante = fabricante;
     }
-
 
     public override void Atualizar(EntidadeBase entidadeAtualizada)
     {
