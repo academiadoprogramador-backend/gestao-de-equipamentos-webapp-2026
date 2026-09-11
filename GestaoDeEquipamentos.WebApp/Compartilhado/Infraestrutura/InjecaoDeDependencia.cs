4c36ba8970944a203b29242c21b9b2bb338326bf
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-09-10 21:03:58 -0300

    Adiciona CRUD de Equipamentos em SQL

--- GestaoDeEquipamentos.WebApp/Compartilhado/Infraestrutura/InjecaoDeDependencia.cs
+++ GestaoDeEquipamentos.WebApp/Compartilhado/Infraestrutura/InjecaoDeDependencia.cs
@@ -1,5 +1,6 @@
 using GestaoDeEquipamentos.WebApp.Compartilhado.Infraestrutura.Arquivos;
 using GestaoDeEquipamentos.WebApp.Modulos.Chamados.Infraestrutura;
+using GestaoDeEquipamentos.WebApp.Modulos.Equipamentos.Dominio;
 using GestaoDeEquipamentos.WebApp.Modulos.Equipamentos.Infraestrutura;
 using GestaoDeEquipamentos.WebApp.Modulos.Fabricantes.Dominio;
 using GestaoDeEquipamentos.WebApp.Modulos.Fabricantes.Infraestrutura;
@@ -31,7 +32,11 @@ public static class InjecaoDeDependencia
             return new RepositorioFabricanteEmSql(connectionString);
         });
 
-        services.AddScoped<RepositorioEquipamentoEmArquivo>();
+        services.AddScoped<IRepositorioEquipamento>(_ =>
+        {
+            return new RepositorioEquipamentoEmSql(connectionString);
+        });
+
         services.AddScoped<RepositorioChamadoEmArquivo>();
     }
 }
---
+++ GestaoDeEquipamentos.WebApp/Modulos/Equipamentos/Infraestrutura/RepositorioEquipamentoEmSql.cs
@@ -0,0 +1,135 @@
+using Dapper;
+using GestaoDeEquipamentos.WebApp.Modulos.Equipamentos.Dominio;
+using Microsoft.Data.SqlClient;
+
+namespace GestaoDeEquipamentos.WebApp.Modulos.Equipamentos.Infraestrutura;
+
+public sealed class RepositorioEquipamentoEmSql : IRepositorioEquipamento
+{
+    private readonly string connectionString;
+
+    public RepositorioEquipamentoEmSql(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public void Cadastrar(Equipamento novoRegistro)
+    {
+        const string query =
+            """
+            INSERT INTO TBEquipamentos (Nome, PrecoAquisicao, DataFabricacao, FabricanteId)
+            OUTPUT INSERTED.Id
+            VALUES (@Nome, @PrecoAquisicao, @DataFabricacao, @FabricanteId)
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        novoRegistro.Id = conexao.QuerySingle<int>(query, new
+        {
+            novoRegistro.Nome,
+            novoRegistro.PrecoAquisicao,
+            novoRegistro.DataFabricacao,
+            FabricanteId = novoRegistro.Fabricante.Id
+        });
+    }
+
+    public bool Editar(int idSelecionado, Equipamento entidadeAtualizada)
+    {
+        const string query =
+            """
+            UPDATE TBEquipamentos
+            SET
+                Nome = @Nome,
+                PrecoAquisicao = @PrecoAquisicao,
+                DataFabricacao = @DataFabricacao,
+                FabricanteId = @FabricanteId
+            WHERE Id = @Id
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        int quantidadeRegistrosAlterados = conexao.Execute(query, new
+        {
+            Id = idSelecionado,
+            entidadeAtualizada.Nome,
+            entidadeAtualizada.PrecoAquisicao,
+            entidadeAtualizada.DataFabricacao,
+            FabricanteId = entidadeAtualizada.Fabricante.Id,
+        });
+
+        return quantidadeRegistrosAlterados == 1;
+    }
+
+    public bool Excluir(int idSelecionado)
+    {
+        const string query = "DELETE FROM TBEquipamentos WHERE Id = @Id";
+
+        using SqlConnection conexao = new(connectionString);
+
+        int quantidadeRegistrosExcluidos = conexao.Execute(query, new { Id = idSelecionado });
+
+        return quantidadeRegistrosExcluidos == 1;
+    }
+
+    public Equipamento? SelecionarPorId(int idSelecionado)
+    {
+        const string query =
+            """
+            SELECT 
+                e.Id,
+                e.Nome,
+                e.PrecoAquisicao,
+                e.DataFabricacao,
+                f.Id,
+                f.Nome,
+                f.Email,
+                f.Telefone 
+            FROM TBEquipamentos e
+            INNER JOIN TBFabricantes f ON f.Id = e.FabricanteId
+            WHERE e.Id = @Id
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        return conexao.Query<Equipamento, Fabricante, Equipamento>(
+            query,
+            MapearEquipamentoCompleto,
+            new { Id = idSelecionado }
+        ).SingleOrDefault();
+    }
+
+    public List<Equipamento> SelecionarTodos()
+    {
+        const string query =
+            """
+            SELECT 
+                e.Id,
+                e.Nome,
+                e.PrecoAquisicao,
+                e.DataFabricacao,
+                f.Id,
+                f.Nome,
+                f.Email,
+                f.Telefone 
+            FROM TBEquipamentos e
+            INNER JOIN TBFabricantes f ON f.Id = e.FabricanteId
+            ORDER BY e.Id
+            """;
+
+        using SqlConnection conexao = new(connectionString);
+
+        return conexao.Query<Equipamento, Fabricante, Equipamento>(
+            query,
+            MapearEquipamentoCompleto
+        ).ToList();
+    }
+
+    private static Equipamento MapearEquipamentoCompleto(
+        Equipamento equipamento,
+        Fabricante fabricante
+    )
+    {
+        equipamento.Fabricante = fabricante;
+        return equipamento;
+    }
+}
